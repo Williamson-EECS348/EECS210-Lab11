38d92e7ef053cdf19ee2f10633f6a3ca9beffec5
Jack <jackwilliamson.1215@gmail.com>
2025-12-01 11:26:17 -0600

    make getPostfixExpr function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,43 @@
+// INSERT COMMENT
 #include <iostream>
+#include <string>
+#include <sstream>
+
+std::string getPostfixExpr() {
+    std::string validOps = "+-*/^";
+    std::string validNums = "123";
+    std::string validVars = "xyz";
+
+    bool loop = false;
+    std::string pfInput{};
+    do {
+        pfInput.clear();
+        std::cout << "Enter an arithmetic expression in postfix form: \n";
+        std::getline(std::cin, pfInput);
+
+        std::stringstream ss(pfInput);
+        std::string word{};
+        while (ss >> word) {
+            bool valid = true;
+            for (char c : word) {
+                if (validOps.find(c)  == std::string::npos &&
+                    validNums.find(c) == std::string::npos &&
+                    validVars.find(c) == std::string::npos) { valid = false; }
+
+            }
+            if (!valid) {
+                std::cout << "Input Invalid\n";
+                loop = true;
+                break;
+            }
+        }
+    } while (loop);
+
+    return pfInput;
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string postfix = getPostfixExpr();
+    std::cout << postfix << std::endl;
 }
